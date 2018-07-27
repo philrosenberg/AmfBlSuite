7a76405408b678de6cb050faeeaf4cb0cac22602
Phil Rosenberg <p.d.rosenberg@gmail.com>
2018-07-27 15:56:09 +0100

    Parse command line arguments for input and output directories

--- LidarQuicklookPlotter/app.cpp
+++ LidarQuicklookPlotter/app.cpp
@@ -5,9 +5,19 @@ IMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
-	wxString inputDirectory = "D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\doppler_lidar_backup_ds1\\Data\\Proc\\2018\\";
-	wxString outputDirectory = "D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\quicklooks\\";
-	mainFrame* frame = new mainFrame(0L, "Lidar quicklook plotter", inputDirectory, outputDirectory, true);
+	wxString inputDirectory;
+	wxString outputDirectory;
+#ifdef _DEBUG
+	inputDirectory = "D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\doppler_lidar_backup_ds1\\Data\\Proc\\2018\\";
+	outputDirectory = "D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\quicklooks\\";
+#endif
+	if (argc == 3)
+	{
+		inputDirectory = wxString(argv[1]);
+		outputDirectory = wxString(argv[2]);
+	}
+
+	mainFrame* frame = new mainFrame(0L, "Lidar quicklook plotter", inputDirectory, outputDirectory, inputDirectory.length()>0 && outputDirectory.length()>0);
 	frame->SetIcon(wxICON(amain));
 	frame->Show();
 	return true;
