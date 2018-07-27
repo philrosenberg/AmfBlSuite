99571ba36602fdcc51a13efbb49cae3ef598a828
Phil Rosenberg <p.d.rosenberg@gmail.com>
2018-07-27 15:44:23 +0100

    Shuffled some throw catch things around
    This just ensures that we flag plotting at exactly the right time,
    fixes a couple of misleading messages that indicate we have completed or
    are continuing when in fact we stopped and also means that if one plot
    throws an error other files will still be plotted.

--- LidarQuicklookPlotter/mainFrame.cpp
+++ LidarQuicklookPlotter/mainFrame.cpp
@@ -217,148 +217,18 @@ std::vector<std::string> getDirectoryListing(const std::string &directory, const
 
 void mainFrame::plot()
 {
-	plot("*Stare_??_????????_??.hpl");
-	plot("*VAD_??_????????_??????.hpl");
-	(*m_progressReporter) << "Generated plots for all files found. Waiting approx 5 mins to check again.\n\n";
-}
-
-//Plot just a specific file type - the last file found alphabetically
-//is assumed to be the last file chronologically (assuming you have a
-//sensible naming structure) and it is assumed that this file may be
-//incomplete, so will be replotted next time just in case.
-void mainFrame::plot(const std::string &filter)
-{
 	if (m_plotting)
 		return;
 	m_plotting = true;
 	//There must be no code that can throw without being caught until m_plotting is set back to false
-
 	try
 	{
-		//ensure that the input and output directories end with a slash or are empty
-		if (m_inputDirectory.length() > 0 && m_inputDirectory.back()!= '/' && m_inputDirectory.back()!= '\\')
-			m_inputDirectory = m_inputDirectory+"/";
-		if (m_outputDirectory.length() > 0 && m_outputDirectory.back() != '/' && m_outputDirectory.back() != '\\')
-			m_outputDirectory = m_outputDirectory + "/";
-
-		std::vector<std::string> plottedFiles;
-		(*m_progressReporter) << "Looking for data files to plot.\n";
-		//check the output directory exists
-		if (!wxDirExists(m_outputDirectory))
-			wxFileName::Mkdir(m_outputDirectory, 770, wxPATH_MKDIR_FULL);
-		if (!wxDirExists(m_outputDirectory))
-		{
-			std::ostringstream message;
-			message << "The output directory " << m_outputDirectory << " does not exist and could not be created.";
-			throw(message.str());
-		}
-
-		//check the input directory exists
-		if(!wxDirExists(m_inputDirectory))
-		{
-			std::ostringstream message;
-			message << "The input directory " << m_inputDirectory << " does not exist.";
-			throw(message.str());
-		}
-
-		//get a list of all the files previously plotted
-		std::vector<std::string> previouslyPlottedFiles;
-		std::string previouslyPlottedFilename = m_outputDirectory + "previouslyPlottedFiles.txt";
-
-		std::fstream fin;
-		fin.open(previouslyPlottedFilename.c_str(), std::ios::in);
-		std::string filename;
-		std::getline(fin, filename);
-		while (filename.length() > 0)
-		{
-			previouslyPlottedFiles.push_back(filename);
-			std::getline(fin, filename);
-		}
-		fin.close();
-
-		//Find all the files in the input directory
-		std::vector<std::string> allFiles = getDirectoryListing(m_inputDirectory, filter);
-
-		//Sort the files in alphabetical order - this will also put them in time order
-		//which is important for hunting out the last file of any type which may have been
-		//incomplete.
-		if(allFiles.size() > 0)
-			std::sort(allFiles.begin(), allFiles.end());
-
-		//Remember the name of the last file in the list - we assume thismay be incomplete
-		//so don't let it get remembered as a previously plotted file
-		std::string lastFileToPlot;
-		if (allFiles.size() > 0)
-			lastFileToPlot = allFiles.back();
-
-		//Filter for just files we are interested in
-		//std::vector<std::string> filesOfInterest;
-		//filesOfInterest.reserve(allFiles.size());
-		//for (size_t i = 0; i < allFiles.size(); ++i)
-		//{
-		//	if (allFiles[i].find("Stare", m_inputDirectory.length()) != std::string::npos)
-		//		filesOfInterest.push_back(allFiles[i]);
-		//	if (allFiles[i].find("VAD", m_inputDirectory.length()) != std::string::npos)
-		//		filesOfInterest.push_back(allFiles[i]);
-		//}
-		//The code above was replaced by wildcard filtering when we hunt for files
-		//however it has been left in place in case we need to do something similar in
-		//the future
-		std::vector<std::string> filesOfInterest = allFiles;
-
-		std::vector<std::string> filesToPlot;
-		filesToPlot.reserve(filesOfInterest.size());
-		for (size_t i = 0; i < filesOfInterest.size(); ++i)
-		{
-			bool alreadyPlotted = false;
-			for (size_t j = 0; j < previouslyPlottedFiles.size(); ++j)
-			{
-				if (filesOfInterest[i] == previouslyPlottedFiles[j])
-				{
-					alreadyPlotted = true;
-					break;
-				}
-			}
-			if (!alreadyPlotted)
-				filesToPlot.push_back(filesOfInterest[i]);
-		}
-
-		if (filesToPlot.size() == 0)
-			(*m_progressReporter) << "Found no new files to plot.\n";
-		else
-		{
-			(*m_progressReporter) << "Found the following new files to plot matching the filter " << filter << " :\n";
-			for(size_t i=0; i<filesToPlot.size(); ++i)
-				(*m_progressReporter) << "\t" << filesToPlot[i] << "\n";
-
-			for (size_t i = 0; i < filesToPlot.size(); ++i)
-			{
-				(*m_progressReporter) << "Processing " << filesToPlot[i] << "\n";
-				std::string outputFile = m_outputDirectory + filesToPlot[i].substr(m_inputDirectory.length(), std::string::npos);
-				plotFile(filesToPlot[i], outputFile, std::numeric_limits<double>::max(), *m_progressReporter, this);
-				if (m_progressReporter->shouldStop())
-				{
-					(*m_progressReporter) << "Operation halted at user request.\n";
-					break;
-				}
-
-				//remember which files have been plotted
-				if (filesToPlot[i] != lastFileToPlot)
-				{
-					std::fstream fout;
-					fout.open(previouslyPlottedFilename.c_str(), std::ios::app);
-					fout << filesToPlot[i] << "\n";
-					fout.close();
-				}
-			}
-
-			//plotFile("../../../data/co/2013/201301/20130124/Stare_05_20130124_16.hpl", "testStare.png", this);
-			//plotFile("../../../data/co/2013/201301/20130124/Wind_Profile_05_20130124_162124.hpl", "testWindProfile.png", this);
-			//plotFile("VAD_18_20180712_090610.hpl", "moccha_clutter_1000.png", 1000, this);
-			//plotFile("D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\doppler_lidar_backup_ds1\\Data\\Proc\\2018\\201807\\20180711\\Stare_18_20180711_20.hpl", "moccha_test_stare_cubehelix.png", 1000, this);
-			//plotFile("D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\doppler_lidar_backup_ds1\\Data\\Proc\\2018\\201807\\20180711\\VAD_18_20180711_081948.hpl", "moccha_test_vad_cubehelix.png", 10000, this);
-		}
-		(*m_progressReporter) << "Generated plots for all files matching filter " << filter << "\n";
+		if(!m_progressReporter->shouldStop())
+			plot("*Stare_??_????????_??.hpl");
+		if (!m_progressReporter->shouldStop())
+			plot("*VAD_??_????????_??????.hpl");
+		if (!m_progressReporter->shouldStop())
+			(*m_progressReporter) << "Generated plots for all files found. Waiting approx 5 mins to check again.\n\n";
 	}
 	catch (sci::err err)
 	{
@@ -402,8 +272,185 @@ void mainFrame::plot(const std::string &filter)
 	}
 
 	m_plotting = false;
-	if(m_progressReporter->shouldStop())
+	if (m_progressReporter->shouldStop())
 		m_logText->AppendText("Stopped\n\n");
+}
+
+//Plot just a specific file type - the last file found alphabetically
+//is assumed to be the last file chronologically (assuming you have a
+//sensible naming structure) and it is assumed that this file may be
+//incomplete, so will be replotted next time just in case.
+void mainFrame::plot(const std::string &filter)
+{
+	//ensure that the input and output directories end with a slash or are empty
+	if (m_inputDirectory.length() > 0 && m_inputDirectory.back()!= '/' && m_inputDirectory.back()!= '\\')
+		m_inputDirectory = m_inputDirectory+"/";
+	if (m_outputDirectory.length() > 0 && m_outputDirectory.back() != '/' && m_outputDirectory.back() != '\\')
+		m_outputDirectory = m_outputDirectory + "/";
+
+	std::vector<std::string> plottedFiles;
+	(*m_progressReporter) << "Looking for data files to plot.\n";
+	//check the output directory exists
+	if (!wxDirExists(m_outputDirectory))
+		wxFileName::Mkdir(m_outputDirectory, 770, wxPATH_MKDIR_FULL);
+	if (!wxDirExists(m_outputDirectory))
+	{
+		std::ostringstream message;
+		message << "The output directory " << m_outputDirectory << " does not exist and could not be created.";
+		throw(message.str());
+	}
+
+	//check the input directory exists
+	if(!wxDirExists(m_inputDirectory))
+	{
+		std::ostringstream message;
+		message << "The input directory " << m_inputDirectory << " does not exist.";
+		throw(message.str());
+	}
+
+	//get a list of all the files previously plotted
+	std::vector<std::string> previouslyPlottedFiles;
+	std::string previouslyPlottedFilename = m_outputDirectory + "previouslyPlottedFiles.txt";
+
+	std::fstream fin;
+	fin.open(previouslyPlottedFilename.c_str(), std::ios::in);
+	std::string filename;
+	std::getline(fin, filename);
+	while (filename.length() > 0)
+	{
+		previouslyPlottedFiles.push_back(filename);
+		std::getline(fin, filename);
+	}
+	fin.close();
+
+	//Find all the files in the input directory
+	std::vector<std::string> allFiles = getDirectoryListing(m_inputDirectory, filter);
+
+	//Sort the files in alphabetical order - this will also put them in time order
+	//which is important for hunting out the last file of any type which may have been
+	//incomplete.
+	if(allFiles.size() > 0)
+		std::sort(allFiles.begin(), allFiles.end());
+
+	//Remember the name of the last file in the list - we assume thismay be incomplete
+	//so don't let it get remembered as a previously plotted file
+	std::string lastFileToPlot;
+	if (allFiles.size() > 0)
+		lastFileToPlot = allFiles.back();
+
+	//Filter for just files we are interested in
+	//std::vector<std::string> filesOfInterest;
+	//filesOfInterest.reserve(allFiles.size());
+	//for (size_t i = 0; i < allFiles.size(); ++i)
+	//{
+	//	if (allFiles[i].find("Stare", m_inputDirectory.length()) != std::string::npos)
+	//		filesOfInterest.push_back(allFiles[i]);
+	//	if (allFiles[i].find("VAD", m_inputDirectory.length()) != std::string::npos)
+	//		filesOfInterest.push_back(allFiles[i]);
+	//}
+	//The code above was replaced by wildcard filtering when we hunt for files
+	//however it has been left in place in case we need to do something similar in
+	//the future
+	std::vector<std::string> filesOfInterest = allFiles;
+
+	std::vector<std::string> filesToPlot;
+	filesToPlot.reserve(filesOfInterest.size());
+	for (size_t i = 0; i < filesOfInterest.size(); ++i)
+	{
+		bool alreadyPlotted = false;
+		for (size_t j = 0; j < previouslyPlottedFiles.size(); ++j)
+		{
+			if (filesOfInterest[i] == previouslyPlottedFiles[j])
+			{
+				alreadyPlotted = true;
+				break;
+			}
+		}
+		if (!alreadyPlotted)
+			filesToPlot.push_back(filesOfInterest[i]);
+	}
+
+	if (filesToPlot.size() == 0)
+		(*m_progressReporter) << "Found no new files to plot.\n";
+	else
+	{
+		(*m_progressReporter) << "Found the following new files to plot matching the filter " << filter << " :\n";
+		for(size_t i=0; i<filesToPlot.size(); ++i)
+			(*m_progressReporter) << "\t" << filesToPlot[i] << "\n";
+
+		for (size_t i = 0; i < filesToPlot.size(); ++i)
+		{
+			(*m_progressReporter) << "Processing " << filesToPlot[i] << "\n";
+			std::string outputFile = m_outputDirectory + filesToPlot[i].substr(m_inputDirectory.length(), std::string::npos);
+			try
+			{
+				plotFile(filesToPlot[i], outputFile, std::numeric_limits<double>::max(), *m_progressReporter, this);
+			}
+			catch (sci::err err)
+			{
+				wxTextAttr originalStyle = m_logText->GetDefaultStyle();
+				m_logText->SetDefaultStyle(wxTextAttr(*wxRED));
+				std::ostream stream(m_logText);
+				stream << "Error: " << err.getErrorCategory() << ":" << err.getErrorCode() << " " << err.getErrorMessage() << "\n";
+				m_logText->SetDefaultStyle(originalStyle);
+			}
+			catch (char *errMessage)
+			{
+				wxTextAttr originalStyle = m_logText->GetDefaultStyle();
+				m_logText->SetDefaultStyle(wxTextAttr(*wxRED));
+				std::ostream stream(m_logText);
+				stream << "Error: " << errMessage << "\n";
+				m_logText->SetDefaultStyle(originalStyle);
+			}
+			catch (std::string errMessage)
+			{
+				wxTextAttr originalStyle = m_logText->GetDefaultStyle();
+				m_logText->SetDefaultStyle(wxTextAttr(*wxRED));
+				std::ostream stream(m_logText);
+				stream << "Error: " << errMessage << "\n";
+				m_logText->SetDefaultStyle(originalStyle);
+			}
+			catch (wxString errMessage)
+			{
+				wxTextAttr originalStyle = m_logText->GetDefaultStyle();
+				m_logText->SetDefaultStyle(wxTextAttr(*wxRED));
+				std::ostream stream(m_logText);
+				stream << "Error: " << errMessage << "\n";
+				m_logText->SetDefaultStyle(originalStyle);
+			}
+			catch (...)
+			{
+				wxTextAttr originalStyle = m_logText->GetDefaultStyle();
+				m_logText->SetDefaultStyle(wxTextAttr(*wxRED));
+				std::ostream stream(m_logText);
+				stream << "Error: Unknown\n";
+				m_logText->SetDefaultStyle(originalStyle);
+			}
+			if (m_progressReporter->shouldStop())
+			{
+				(*m_progressReporter) << "Operation halted at user request.\n";
+				break;
+			}
+
+			//remember which files have been plotted
+			if (filesToPlot[i] != lastFileToPlot)
+			{
+				std::fstream fout;
+				fout.open(previouslyPlottedFilename.c_str(), std::ios::app);
+				fout << filesToPlot[i] << "\n";
+				fout.close();
+			}
+		}
+
+		//plotFile("../../../data/co/2013/201301/20130124/Stare_05_20130124_16.hpl", "testStare.png", this);
+		//plotFile("../../../data/co/2013/201301/20130124/Wind_Profile_05_20130124_162124.hpl", "testWindProfile.png", this);
+		//plotFile("VAD_18_20180712_090610.hpl", "moccha_clutter_1000.png", 1000, this);
+		//plotFile("D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\doppler_lidar_backup_ds1\\Data\\Proc\\2018\\201807\\20180711\\Stare_18_20180711_20.hpl", "moccha_test_stare_cubehelix.png", 1000, this);
+		//plotFile("D:\\OneDrive\\Documents\\Work\\Leeds\\MOCCHA\\Mob data\\doppler_lidar_backup_ds1\\Data\\Proc\\2018\\201807\\20180711\\VAD_18_20180711_081948.hpl", "moccha_test_vad_cubehelix.png", 10000, this);
+	}
+	if(!m_progressReporter->shouldStop())
+		(*m_progressReporter) << "Generated plots for all files matching filter " << filter << "\n";
+	
 }
 
 void mainFrame::OnAbout(wxCommandEvent& event)
