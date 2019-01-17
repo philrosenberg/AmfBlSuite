4ee4c30c67182284c516bcdc4ba1e5a40e69824c
Phil Rosenberg <p.d.rosenberg@gmail.com>
2019-01-17 13:09:46 +0000

    Moved the different lidar processors to their own cpp files
    For now there is just the wind profile and stare ones.

---
+++ LidarQuicklookPlotter/LidarStareProcessor.cpp
@@ -0,0 +1,15 @@
+#define _USE_MATH_DEFINES
+#include"Lidar.h"
+#include<fstream>
+#include"Units.h"
+#include<cmath>
+#include"Plotting.h"
+#include<svector\splot.h>
+
+void LidarStareProcessor::readData(const sci::string &inputFilename, ProgressReporter &progressReporter, wxWindow *parent)
+{
+}
+
+void LidarStareProcessor::plotData(const sci::string &outputFilename, const std::vector<metre> maxRanges, ProgressReporter &progressReporter, wxWindow *parent)
+{
+}
