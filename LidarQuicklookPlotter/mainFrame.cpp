1d370c64dac1f97512ff4cd8d8fc8bccf3084e7e
Phil Rosenberg <p.d.rosenberg@gmail.com>
2019-08-15 15:16:47 +0100

    Incremented version

--- LidarQuicklookPlotter/mainFrame.cpp
+++ LidarQuicklookPlotter/mainFrame.cpp
@@ -57,7 +57,7 @@ mainFrame::mainFrame(wxFrame *frame, const wxString& title, const wxString &sett
 	setSetupFile(sci::fromWxString(settingsFile));
 
 	m_processingSoftwareInfo.url = sU("https://github.com/philrosenberg/AmfBlSuite.git");
-	m_processingSoftwareInfo.version = sU("1.1.0");
+	m_processingSoftwareInfo.version = sU("1.1.1");
 
 	wxMenuBar* mbar = new wxMenuBar();
 	wxMenu* fileMenu = new wxMenu(wxT(""));
